5ba2930f79a495766e49694e15eb6a7460d102c2
dernasherbrezon <rodionovamp@mail.ru>
2022-06-15 15:15:54 +0100

    support compilation flags

---
+++ configure_flags.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+CPU=$1
+
+if [ "${CPU}" = "arm1176jzf-s" ]; then
+   export CXXFLAGS="-mcpu=${CPU} -mfpu=vfp -mfloat-abi=hard"
+elif [ "${CPU}" = "cortex-a53" ]; then
+   export CXXFLAGS="-mcpu=${CPU} -mfpu=neon-fp-armv8 -mfloat-abi=hard"
+elif [ "${CPU}" = "cortex-a7" ]; then
+   export CXXFLAGS="-mcpu=${CPU} -mfpu=neon-vfpv4 -mfloat-abi=hard"
+elif [ "${CPU}" = "cortex-a72" ]; then
+   export CXXFLAGS="-mcpu=${CPU} -mfpu=neon-fp-armv8 -mfloat-abi=hard"
+elif [ "${CPU}" = "generic" ]; then
+   export CXXFLAGS=""
+else
+   echo "unknown core: ${CPU}"
+   exit 1
+fi
+
+if [[ "${CPU}" = "generic" ]]; then
+   export BUCKET=r2cloud
+else
+   export BUCKET="r2cloud/cpu-${CPU}"
+fi
+
+export ASMFLAGS="${CXXFLAGS} -mthumb -g"
+export CFLAGS=${CXXFLAGS}
