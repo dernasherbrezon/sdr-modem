1b3cb0879600413b21835949efc1c72dd3980dce
dernasherbrezon <rodionovamp@mail.ru>
2022-06-25 10:11:45 +0100

    switch over to manual sin/cos
    volk on generic amd64 produce incorrect sin + fix phase

--- src/dsp/frequency_modulator.c
+++ src/dsp/frequency_modulator.c
@@ -8,18 +8,11 @@
 #define M_PI 3.14159265358979323846
 #endif
 
+#define M_2PI ((float) (2 * M_PI))
+
 struct frequency_modulator_t {
     float phase;
     float sensitivity;
-
-    float *temp;
-    size_t temp_len;
-
-    float *sin;
-    size_t sin_len;
-
-    float *cos;
-    size_t cos_len;
 
     float complex *output;
     size_t output_len;
@@ -41,26 +34,6 @@ int frequency_modulator_create(float sensitivity, uint32_t max_input_buffer_leng
         frequency_modulator_destroy(result);
         return -ENOMEM;
     }
-    result->sin_len = max_input_buffer_length;
-    result->sin = malloc(sizeof(float) * result->sin_len);
-    if (result->sin == NULL) {
-        frequency_modulator_destroy(result);
-        return -ENOMEM;
-    }
-    result->cos_len = max_input_buffer_length;
-    result->cos = malloc(sizeof(float) * result->cos_len);
-    if (result->cos == NULL) {
-        frequency_modulator_destroy(result);
-        return -ENOMEM;
-    }
-    result->temp_len = max_input_buffer_length;
-    result->temp = malloc(sizeof(float) * result->temp_len);
-    if (result->temp == NULL) {
-        frequency_modulator_destroy(result);
-        return -ENOMEM;
-    }
-
-
     *mod = result;
     return 0;
 }
@@ -74,14 +47,14 @@ void frequency_modulator_process(float *input, size_t input_len, float complex *
     }
     for (size_t i = 0; i < input_len; i++) {
         mod->phase = mod->phase + mod->sensitivity * input[i];
-        mod->temp[i] = mod->phase;
+        if (mod->phase < -M_2PI) {
+            mod->phase += M_2PI;
+        }
+        if (mod->phase > M_2PI) {
+            mod->phase -= M_2PI;
+        }
+        mod->output[i] = cos(mod->phase) + I * sin(mod->phase);
     }
-
-    volk_32f_sin_32f(mod->sin, mod->temp, (unsigned int) input_len);
-    volk_32f_cos_32f(mod->cos, mod->temp, (unsigned int) input_len);
-
-    volk_32f_x2_interleave_32fc(mod->output, mod->cos, mod->sin, (unsigned int) input_len);
-
     *output = mod->output;
     *output_len = input_len;
 }
@@ -92,15 +65,6 @@ void frequency_modulator_destroy(frequency_modulator *mod) {
     }
     if (mod->output != NULL) {
         free(mod->output);
-    }
-    if (mod->temp != NULL) {
-        free(mod->temp);
-    }
-    if (mod->sin != NULL) {
-        free(mod->sin);
-    }
-    if (mod->cos != NULL) {
-        free(mod->cos);
     }
     free(mod);
 }
