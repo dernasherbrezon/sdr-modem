7cd02374be5f266c0abfd6135a2df78cc26ea0da
dernasherbrezon <rodionovamp@mail.ru>
2022-06-29 19:54:50 +0100

    fix path during debug

--- configure_flags.sh
+++ configure_flags.sh
@@ -3,17 +3,17 @@
 CPU=$1
 
 if [ "${CPU}" = "arm1176jzf-s" ]; then
-   CXXFLAGS="-g -O2 -fdebug-prefix-map=$(pwd) -mcpu=${CPU} -mfpu=vfp -mfloat-abi=hard"
+   CXXFLAGS="-g -O2 -fdebug-prefix-map=$(pwd)=. -mcpu=${CPU} -mfpu=vfp -mfloat-abi=hard"
 elif [ "${CPU}" = "cortex-a53" ]; then
-   CXXFLAGS="-g -O2 -fdebug-prefix-map=$(pwd) -mcpu=${CPU} -mfpu=neon-fp-armv8 -mfloat-abi=hard"
+   CXXFLAGS="-g -O2 -fdebug-prefix-map=$(pwd)=. -mcpu=${CPU} -mfpu=neon-fp-armv8 -mfloat-abi=hard"
 elif [ "${CPU}" = "cortex-a7" ]; then
-   CXXFLAGS="-g -O2 -fdebug-prefix-map=$(pwd) -mcpu=${CPU} -mfpu=neon-vfpv4 -mfloat-abi=hard"
+   CXXFLAGS="-g -O2 -fdebug-prefix-map=$(pwd)=. -mcpu=${CPU} -mfpu=neon-vfpv4 -mfloat-abi=hard"
 elif [ "${CPU}" = "cortex-a72" ]; then
-   CXXFLAGS="-g -O2 -fdebug-prefix-map=$(pwd) -mcpu=${CPU} -mfpu=neon-fp-armv8 -mfloat-abi=hard"
+   CXXFLAGS="-g -O2 -fdebug-prefix-map=$(pwd)=. -mcpu=${CPU} -mfpu=neon-fp-armv8 -mfloat-abi=hard"
 elif [ "${CPU}" = "generic" ]; then
-   CXXFLAGS="-g -O2 -fdebug-prefix-map=$(pwd)"
+   CXXFLAGS="-g -O2 -fdebug-prefix-map=$(pwd)=."
 elif [ "${CPU}" = "nocpuspecific" ]; then
-   CXXFLAGS="-g -O2 -fdebug-prefix-map=$(pwd)"
+   CXXFLAGS="-g -O2 -fdebug-prefix-map=$(pwd)=."
 else
    echo "unknown core: ${CPU}"
    exit 1
