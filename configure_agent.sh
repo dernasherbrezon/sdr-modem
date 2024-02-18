7ca115f033fafffe8fe7685ebf352d928a2235fb
dernasherbrezon <dernasherbrezon@gmail.com>
2024-02-18 19:25:48 +0000

    fixing jenkins build

--- configure_agent.sh
+++ configure_agent.sh
@@ -10,7 +10,7 @@ sudo bash -c "echo \"deb http://r2cloud.s3.amazonaws.com/cpu-generic $(lsb_relea
 sudo apt-get update
 sudo apt-get install -y debhelper git-buildpackage cmake libvolk2-dev libprotobuf-c-dev libconfig-dev check libiio-dev
 
-APT_CLI_VERSION="apt-cli-1.9"
+APT_CLI_VERSION="apt-cli-1.8"
 if [ ! -f ${HOME}/${APT_CLI_VERSION}.jar ]; then
   wget -O ${APT_CLI_VERSION}.jar.temp https://github.com/dernasherbrezon/apt-cli/releases/download/${APT_CLI_VERSION}/apt-cli.jar
   mv ${APT_CLI_VERSION}.jar.temp ${HOME}/${APT_CLI_VERSION}.jar
