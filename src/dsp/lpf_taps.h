a065be2aa4f182808ad3e349297b901f1c23c94c
dernasherbrezon <dernasherbrezon@gmail.com>
2026-08-16 19:37:38 +0100

    remove unused

--- src/dsp/lpf_taps.h
+++
@@ -1,8 +0,0 @@
-#ifndef SRC_LPF_H_
-#define SRC_LPF_H_
-
-#include <stdint.h>
-
-int create_low_pass_filter(float gain, uint64_t sampling_freq, uint64_t cutoff_freq, uint32_t transition_width, float **taps, size_t *len);
-
-#endif /* SRC_LPF_H_ */
